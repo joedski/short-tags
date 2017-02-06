0dd3bee7a4890f6c813172a6422ab510390f2823
Joseph Sikorski <joedski@gmail.com>
2017-02-05 21:36:18 -0500

    Initial code commit

---
+++ src/lexer/index.pegjs
@@ -0,0 +1,187 @@
+/**
+ * A lexer to parse the raw text into nodes:
+ * - text
+ * - tagOpen
+ * - tagClose
+ */
+
+/**
+ * Type supplied by PEGJS:
+ *
+ * Location = {
+ *   start: {
+ *     column: number,
+ *     line: number,
+ *     offset: number,
+ *   },
+ *   end: {
+ *     column: number,
+ *     line: number,
+ *     offset: number,
+ *   },
+ * }
+ */
+
+/**
+ * Corpus = Array<TagClose | TagOpen | Text>
+ */
+Corpus
+	= entities:(TagClose / TagOpen / Text)* {
+		return entities;
+	}
+
+
+
+//// Text
+
+/**
+ * Text = {
+ *   type: 'text',
+ *   text: string,
+ *   location: Location,
+ * }
+ */
+Text
+	= chars:(NonOpeningChar / EscapedChar)+ {
+		return {
+			type: "text",
+			text: chars.join( '' ),
+			location: location(),
+		};
+	}
+
+NonOpeningChar
+	= [^\{\\]
+
+// Note: Not sure if should error or just let you put a backslash at the end.
+EscapedChar
+	= "\\" c:.? {
+		return c;
+	}
+
+
+
+//// Space
+
+Space
+	= $ [ \t\r\n]+
+
+
+
+//// TagOpen
+
+/**
+ * TagOpen = {
+ *   type: 'tagOpen',
+ *   tagName: string,
+ *   attributes: Map<string, string>,
+ *   text: string,
+ *   location: Location
+ * }
+ */
+TagOpen
+	= "{" tagName:TagNameWithShortcuts attrs:(Space Attribute)* Space? closingSlash:"/"? "}" {
+		// Setting an explicit attribute WILL override the shortcuts.
+		var attributes = new Map(attrs.map(function( a ) { return a[1]; }));
+
+		if( tagName.id && ! attributes.has( 'id' ) ) {
+			attributes.set( 'id', tagName.id );
+		}
+
+		if( tagName.value && ! attributes.has( 'value' ) ) {
+			attributes.set( 'value', tagName.value );
+		}
+
+		return {
+			type: closingSlash ? "tagSelfClosing" : "tagOpen",
+			tagName: tagName.name,
+			attributes: attributes,
+      text: text(),
+			location: location(),
+		};
+	}
+
+TagNameWithShortcuts
+	= name:($ TagNameValueChars+) shortcuts:(IdShortcut / ValueShortcut)* {
+		var id, value;
+
+		shortcuts.forEach(function( s ) {
+			if( s.type === 'id' ) id = s.value;
+			else if( s.type === 'value' ) value = s.value;
+		});
+
+		return {
+			name: name,
+			id: id,
+			value: value,
+		};
+	}
+
+IdShortcut
+	= "#" value:($ TagNameValueChars*) {
+		return {
+			type: 'id',
+			value: value,
+		};
+	}
+
+ValueShortcut
+	= "=" value:($ TagNameValueChars*) {
+		return {
+			type: 'value',
+			value: value,
+		};
+	}
+
+TagNameValueChars = [^/ }=#]
+
+
+
+//// TagClose
+
+/**
+ * TagClose = {
+ *   type: 'tagClose',
+ *   tagName: string,
+ *   text: string,
+ *   location: Location,
+ * }
+ */
+TagClose
+	= "{/" tagName:TagNameWithShortcuts "}" {
+		// NOTE: You can include shortcuts in the closing tag, but they will be ignored.
+		return {
+			type: "tagClose",
+			tagName: tagName.name,
+			location: location(),
+			text: text(),
+		};
+	}
+
+
+
+//// Attributes
+
+Attribute = (KeyValueAttribute / BooleanAttribute)
+
+KeyValueAttribute
+	= name:AttributeName "=" value:AttributeValue {
+		return [ name, value ];
+	}
+
+BooleanAttribute
+	= name:AttributeName {
+		return [ name, true ];
+	}
+
+AttributeValue
+	= QuotedAttributeValue / UnquotedAttributeValue
+
+AttributeName
+	= $ [^/ }=]+
+
+UnquotedAttributeValue
+	= $ [^/ }'"]*
+
+QuotedAttributeValue
+	= value:('"' $[^"]* '"' / "'" $[^']* "'") { return value[1]; }
---
+++ src/treeifier/index.js
@@ -0,0 +1,189 @@
+// @flow
+
+
+type Location = {
+  start: {
+    column: number,
+    line: number,
+    offset: number,
+  },
+  end: {
+    column: number,
+    line: number,
+    offset: number,
+  },
+};
+
+type TextEntity = {
+  type: 'text',
+  text: string,
+  location: Location,
+};
+
+type TagOpenEntity = {
+  type: 'tagOpen',
+  tagName: string,
+  attributes: Map<string, string>,
+  text: string,
+  location: Location
+};
+
+type TagSelfClosingEntity = {
+  type: 'tagSelfClosing',
+  tagName: string,
+  attributes: Map<string, string>,
+  text: string,
+  location: Location
+};
+
+type TagCloseEntity = {
+  type: 'tagClose',
+  tagName: string,
+  text: string,
+  location: Location,
+};
+
+type Entity = TagOpenEntity | TagCloseEntity | TagSelfClosingEntity | TextEntity;
+
+type TagEntity = {
+  type: 'tag',
+  tagName: string,
+  attributes: Map<string, string>,
+  children: Tree,
+  tagOpen: TagOpenEntity,
+  // Tags don't necessarily have a closing tag.
+  // This is especially true for things like document references.
+  tagClose: ?TagCloseEntity,
+  // True if children is empty.
+  empty: boolean,
+  location: Location,
+  contentsLocation: Location,
+};
+
+// TagCloseEntity can get left in if no TagOpenEntity takes it up.
+// The next stage will either output it as text or more likely ignore it.
+// Maybe flag it as an error.
+type Tree = Array<TextEntity | TagEntity | TagCloseEntity>;
+
+type StackFrame = {
+  tree: Tree,
+  list: Array<Entity>,
+  tagOpen?: TagOpenEntity,
+};
+
+
+export function createTag({
+  openTag: TagOpenEntity,
+  closeTag?: TagCloseEntity,
+  contents: Tree = [],
+}): TagEntity {
+  return {
+    type: 'tag',
+    tagName: openTag.tagName,
+    attributes: openTag.attributes,
+    children: contents,
+    tagOpen,
+    tagClose,
+    empty: contents.length === 0,
+    location: {
+      start: openTag.location.start,
+      end: closeTag ? closeTag.location.end : openTag.location.end,
+    },
+    contentsLocation: {
+      start: openTag.location.end,
+      end: closeTag ? closeTag.location.start : openTag.location.end,
+    },
+  };
+}
+
+export default function treeify(entities: Array<Entity>): Tree {
+  const stack = [
+    { tree: [], list: entities },
+  ];
+
+  function peek() { return stack[ stack.length - 1 ]; }
+  function pop() { return stack.pop(); }
+  function push( frame ) { stack.push( frame ); return peek(); }
+  function swap( frame ) { pop(); return push( frame ); }
+
+  while( peek().list.length ) {
+    const { tree, list, openTag } = peek();
+    const [ entity, ...rest ] = list;
+
+    switch( entity.type ) {
+      case 'text': {
+        swap({
+          list: rest,
+          tree: [ ...tree, entity ],
+          openTag,
+        });
+        break;
+      }
+
+      case 'tagOpen': {
+        push({
+          list: rest,
+          tree: [],
+          openTag: entity,
+        });
+        break;
+      }
+
+      case 'tagClose': {
+        if( openTag && openTag.tagName === entity.tagName ) {
+          pop();
+          const { tree: outerTree, openTag: outerOpenTag } = peek();
+          const tag = createTag({ openTag, closeTag: entity, contents: tree });
+          swap({
+            list: rest,
+            tree: [ ...outerTree, tag ],
+            openTag: outerOpenTag,
+          });
+        }
+        else {
+          swap({
+            list: rest,
+            tree: [ ...tree, entity ],
+            openTag,
+          });
+        }
+
+        break;
+      }
+
+      case 'tagSelfClosing': {
+        const tag = createTag({ openTag: entity });
+        swap({
+          list: rest,
+          tree: [ ...tree, tag ],
+          openTag,
+        });
+        break;
+      }
+
+      default: {
+        console.warn( `unrecognized entity of type ${entity.type}:`, entity );
+        swap({
+          list: rest,
+          tree: [ ...tree, entity ],
+          openTag,
+        });
+      }
+    }
+  }
+
+  // Lastly, flatten any unclosed openTags.
+  while( stack.length > 1 ) {
+    const { openTag, tree } = pop();
+    const { openTag: outerOpenTag, tree: outerTree } = pop();
+    const emptyTag = createTag({ openTag });
+
+    push({
+      // We can just discard list because we're done with it.
+      openTag: outerOpenTag,
+      tree: [ ...outerTree, emptyTag, ...tree ],
+    });
+  }
+
+  return peek().tree;
+}
